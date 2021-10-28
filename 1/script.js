4e939496c962e44a6e1a6de0248be575852c8fdb
Alex Satur <alex.satur@and.digital>
2021-10-28 09:54:22 +0100

    in ruby

---
+++ 1/script.js
@@ -0,0 +1,10 @@
+// const answer = new Set(process.argv[2].reduce((a, b) => {
+//   a.push(...[...Array(b[1]).keys()].slice(b[0]))
+//   return a
+// }, [])).size
+//
+// console.log(answer)
+
+// console.log(JSON.parse(process.argv))
+
+console.log(Array.from('01'))
